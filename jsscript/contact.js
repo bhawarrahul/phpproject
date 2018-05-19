57b27debf73c8519a0d7675fd33915a3d8c6059b
Rahul <Rahul@Rahul-PC>
2018-05-19 14:40:43 +0530

    update Contact Form

--- jsscript/contact.js
+++ jsscript/contact.js
@@ -18,25 +18,24 @@ $(function () {
             $.ajax({
                 type: "POST",
                 url: url,
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
                 data: $(this).serialize(),
                 success: function (data)
                 {
                     // data = JSON object that contact.php returns
 
                     // we recieve the type of the message: success x danger and apply it to the 
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
+                    alert(data);
 
-                    // let's compose Bootstrap alert box HTML
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    
-                    // If we have messageAlert and messageText
-                    if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
-                        $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
-                        $('#contact-form')[0].reset();
-                    }
+                    // empty the form
+                    $('#contact-form')[0].reset();
+                },
+                error: function (jqXHR, status, err) {
+                    alert(data);
+
+                    // empty the form
+                    $('#contact-form')[0].reset();
                 }
             });
             return false;
--- sendEnquiry.php
+++ sendEnquiry.php
@@ -1,11 +1,65 @@
-<?php
+
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
-echo '<script language="javascript">';
-echo 'alert("message successfully sent")';
-echo '</script>';
+
+
+<?php
+require("PHPMailer-master/src/PHPMailer.php");
+require("PHPMailer-master/src/SMTP.php");
+require("PHPMailer-master/src/Exception.php");
+date_default_timezone_set('America/Toronto');
+
+//require_once('class.phpmailer.php');
+//include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+;
+
+$body = "gdssdh";
+//$body             = eregi_replace("[\]",'',$body);
+
+$mail->IsSMTP(); // telling the class to use SMTP
+//$mail->Host       = "ssl://smtp.gmail.com"; // SMTP server
+$mail->SMTPDebug = 1;                     // enables SMTP debug information (for testing)
+// 1 = errors and messages
+// 2 = messages only
+$mail->SMTPAuth = true;                  // enable SMTP authentication
+$mail->SMTPSecure = "ssl";                 // sets the prefix to the servier
+$mail->Host = "smtp.gmail.com";      // sets GMAIL as the SMTP server
+$mail->Port = 465;                   // set the SMTP port for the GMAIL server
+$mail->Username = "bhawar.rahul7@gmail.com";  // GMAIL username
+$mail->Password = "9923830564@r";            // GMAIL password
+
+$mail->SetFrom('bhawar.rahul7@gmail.com', 'PRSPS');
+
+//$mail->AddReplyTo("user2@gmail.com', 'First Last");
+
+$mail->Subject = "PRSPS password";
+
+//$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+
+$mail->MsgHTML($body);
+
+$address = "bhawar.rahul7@gmail.com";
+$mail->AddAddress($address, "user2");
+
+//$mail->AddAttachment("images/phpmailer.gif");      // attachment
+//$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
+$ReturnMsg = "";
+if (!$mail->Send()) {
+    $ReturnMsg = "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    $ReturnMsg = "Message sent!";
+}
+$data->success= true;
+$data->error = false;
+$data->msg= $ReturnMsg;
+header('Content-Type: application/json');
+echo json_encode($data);
+?>
+
